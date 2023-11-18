c43a8b3bc5496e97be971b38d2a99659c6d98942
Annemarie535257 <a.twagiraye@alustudent.com>
2023-11-18 11:53:24 -0800

    hard

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -1,100 +1,55 @@
 #!/usr/bin/python3
-'''Defines a Square class'''
+"""Define a class Square."""
 
 
 class Square:
-    '''Square represents an instance of a square shape'''
+    """Represent a square."""
 
-    def __init__(self, size=0, position=(0, 0)):
-        '''Initializes a new square object
+    def __init__(self, size=0):
+        """Initialize a new square.
 
-           Args:
-                size (int): The size of the square.
-        '''
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
-        self.position = position
-
-    def __str__(self):
-        '''
-        Print a rapresentation of square using my_print
-        '''
-        '''
-           Prints graphic representaion of square instance
-        '''
-        if self.size == 0:
-            return ("")
-
-        res = "\n" * self.position[1]
-        for i in range(self.size):
-            res += " " * self.position[0]
-            res += "#" * self.size
-            if i != self.size - 1:
-                res += "\n"
-        return res
-
-    def area(self):
-        '''
-           Computes the area of the square based on the size.
-
-           Returns:
-               The value of the area (int).
-        '''
-        return self.__size ** 2
 
     @property
     def size(self):
-        '''
-           Retrieves the value of size
-        '''
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        '''
-           Sets the value of private attribute size
-        '''
-        if (type(value) != int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if (value < 0):
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @property
-    def position(self):
-        '''
-           Retrieves the current position of square instance.
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
-           Returns:
-               The value of position.
-        '''
-        return self.__position
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
+        return self.area() == other.area()
 
-    @position.setter
-    def position(self, value):
-        '''
-           Sets the value for position and performs type chacks
+    def __ne__(self, other):
+        """Define the != comparison to a Square."""
+        return self.area() != other.area()
 
-           Args:
-             value (tuple): The position of the square
-        '''
-        if (
-                isinstance(value, tuple) and
-                len(value) == 2 and
-                all(map(lambda i: (isinstance(i, int) and
-                                   i >= 0), value))
-        ):
-            self.__position = value
-        else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+    def __lt__(self, other):
+        """Define the < comparison to a Square."""
+        return self.area() < other.area()
 
-    def my_print(self):
-        '''
-           Prints graphic representaion of square instance
-        '''
-        if self.size == 0:
-            print("")
-            return
+    def __le__(self, other):
+        """Define the <= comparison to a Square."""
+        return self.area() <= other.area()
 
-        print("\n" * self.position[1], end="")
-        for i in range(self.size):
-            print(" " * self.position[0] + "#" * self.size)
+    def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
+        return self.area() >= other.area()
