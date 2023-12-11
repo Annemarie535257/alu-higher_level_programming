0a1df5dae7f4aa72a22e8b91fb321825e37a3e06
Annemarie535257 <a.twagiraye@alustudent.com>
2023-12-11 09:31:36 -0800

    done

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""This is the State module.
-Contains the State class that inherits from Base = declarative_base()
-"""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+'''Using the SQL Alchemy in defining a database class'''
 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 Base = declarative_base()
 
 
-class State(Base):
-    """This class links to the `states` table of our database.
-    Attributes:
-        id (int): id of the state.
-        name (str): name of the state.
-    """
+class City(Base):
+    '''A class defination of a table in the sql'''
+    __tablename__ = 'cities'
 
-    __tablename__ = 'states'
-
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
